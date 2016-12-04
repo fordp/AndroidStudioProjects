55061ab889965cefecf03d32beade3baf2f437ad
Glen Muras <gmuras@4wdsupacentre.com.au>
2016-12-05 09:37:52 +1100

    First commit, again.

---
+++ StonehengeGuide/app/src/main/java/com/example/gmuras/stonehengeguide/ImageActivity.java
@@ -0,0 +1,23 @@
+package com.example.gmuras.stonehengeguide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+
+
+    public void returnToMainActivity(View v) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
