aaaa70778c414f3b8cfaeb0f7c0e34974a08ad67
Glen Muras <gmuras@4wdsupacentre.com.au>
2017-02-13 09:49:58 +1100

    Fragment Example Menu - Some changes.

--- FragmentExampleMenu/app/src/main/java/com/example/gmuras/fragmentexamplemenu/MainActivity.java
+++ FragmentExampleMenu/app/src/main/java/com/example/gmuras/fragmentexamplemenu/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gmuras.fragmentexamplemenu;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,11 +42,19 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Fragment fragment;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_date:
+                startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
+                break;
+            case R.id.menu_location:
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.menu_sleep:
+                startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+                break;
         }
 
         return super.onOptionsItemSelected(item);
