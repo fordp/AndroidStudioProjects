b928beb89bec57441ba26bb4d61a7c71cddfbbb0
Glen Muras <gmuras@4wdsupacentre.com.au>
2017-02-21 13:42:49 +1100

    Main menu stuff working basic state.

--- FragmentExampleMenuClock/app/src/main/java/com/example/gmuras/fragmentexample/MainActivity.java
+++ FragmentExampleMenuClock/app/src/main/java/com/example/gmuras/fragmentexample/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.gmuras.fragmentexample;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +24,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         analogButton.setOnClickListener(this);
         digitalButton = (Button) findViewById(R.id.button_digital);
         digitalButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Fragment fragment;
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_date:
+                startActivity(new Intent(android.provider.Settings.ACTION_DATE_SETTINGS));
+                break;
+            case R.id.menu_location:
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.menu_sleep:
+                startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
