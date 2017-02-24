8ca0c02919590f7d66d489e1335cfd778938eaa6
Glen Muras <gmuras@4wdsupacentre.com.au>
2017-02-24 15:48:50 +1100

    Added ancient britain

---
+++ AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gmuras.ancientbritain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
