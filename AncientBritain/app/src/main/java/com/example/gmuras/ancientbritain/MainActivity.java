a9773b1c03ad94b55b93ca4fa3c5d44ff768fc95
Glen <fordp42@gmail.com>
2017-02-26 10:28:57 +1100

    Basic changes.

--- AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainActivity.java
+++ AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.gmuras.ancientbritain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    public static int currentItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
---
+++ AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainDataDef.java
@@ -0,0 +1,8 @@
+package com.example.gmuras.ancientbritain;
+
+/**
+ * Created by Fordp on 26/02/2017.
+ */
+
+public class MainDataDef {
+}
