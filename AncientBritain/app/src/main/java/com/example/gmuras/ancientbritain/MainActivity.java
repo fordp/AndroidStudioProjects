521c6e0979b6c38d45a237aae7a9be7cd712bfc0
Glen Muras <gmuras@4wdsupacentre.com.au>
2017-02-27 10:03:59 +1100

    Almost done with this one.

--- AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainActivity.java
+++ AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.gmuras.ancientbritain;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
     public static int currentItem;
+    static View.OnClickListener mainOnClickListener;
+    private RecyclerView recyclerView;
 
 
     @Override
@@ -20,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<MainDataDef> mainData = new ArrayList<MainDataDef>();
+        for (int i = 0; i < MainData.nameArray.length; i++) {
+            mainData.add(new MainDataDef(
+                    MainData.imageArray[i],
+                    MainData.nameArray[i],
+                    MainData.infoArray[i]
+            ));
+        }
+
+        RecyclerView.Adapter adapter = new MainAdapter(mainData);
+        recyclerView.setAdapter(adapter);
     }
 }
--- AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainDataDef.java
+++ AncientBritain/app/src/main/java/com/example/gmuras/ancientbritain/MainDataDef.java
@@ -5,4 +5,19 @@ package com.example.gmuras.ancientbritain;
  */
 
 public class MainDataDef {
+    int image;
+    String name;
+    String info;
+
+    public MainDataDef(int image, String name, String info) {
+        this.image = image;
+        this.name = name;
+        this.info = info;
+    }
+
+    public int getImage() { return image; }
+
+    public String getName() { return name; }
+
+    public String getInfo() { return info; }
 }
