fbcf594e82288ae64dd7d70973a71c05fb20d066
Glen <fordp42@gmail.com>
2017-04-23 22:00:37 +1000

    Last changes for the gesture stuff. Splash screen working.

--- AncientBritainGesture/app/src/main/java/com/example/gmuras/ancientbritaingesture/SplashActivity.java
+++ AncientBritainGesture/app/src/main/java/com/example/gmuras/ancientbritaingesture/SplashActivity.java
@@ -1,29 +1,62 @@
 package com.example.gmuras.ancientbritaingesture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ImageView imageView = (ImageView) findViewById(R.id.splash_image);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        detector = new GestureDetector(this, new SplashListener());
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+
+
+    private class SplashListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+    }
 }
+
